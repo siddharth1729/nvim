740b0737ec05b346aa635f34ff56c3d44d1ecc3e
siddharth1729 <siddharth.sabron@gmail.com>
2025-04-13 19:54:53 +0530

    DEV: Adding diffrent tags in presence lua

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -15,7 +15,7 @@ return {
     show_time = true,
 
     -- Rich Presence text options
-    editing_text = "Editing %s",
+    editing_text = "Coding %s",
     file_explorer_text = "Browsing %s",
     git_commit_text = "Committing changes",
     plugin_manager_text = "Managing plugins",
