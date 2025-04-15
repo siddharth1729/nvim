8bcdb13a89af15e27840f7a986d7a09e8311eb11
siddharth1729 <siddharth.sabron@gmail.com>
2025-04-15 15:22:42 +0530

    DEV: Adding telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
+  version = false, -- telescope did only one release, so use HEAD for now
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      cond = function()
+        return vim.fn.executable("cmake") == 1
+      end,
+    },
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
+    { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands" },
+  },
+  opts = {
+    defaults = {
+      prompt_prefix = " ",
+      selection_caret = " ",
+      path_display = { "smart" },
+      file_ignore_patterns = { ".git/", "node_modules" },
+      mappings = {
+        i = {
+          ["<C-n>"] = "move_selection_next",
+          ["<C-p>"] = "move_selection_previous",
+          ["<C-j>"] = "move_selection_next",
+          ["<C-k>"] = "move_selection_previous",
+        },
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+      live_grep = {
+        additional_args = function()
+          return { "--hidden" }
+        end,
+      },
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    -- Only load fzf if it's available
+    pcall(telescope.load_extension, "fzf")
+  end,
+} 
